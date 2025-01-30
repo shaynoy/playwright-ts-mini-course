3139e1ec04e4b46aefb83d4b804bc94df6a9a272
Shay Noy <shaynoy@gmail.com>
2025-01-30 23:23:59 +0200

    Hirbush code

--- tests/sanity.spec.ts
+++ tests/sanity.spec.ts
@@ -2,10 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
-  await page.locator('[data-test="username"]').click();
-  await page.locator('[data-test="username"]').fill('standard_user');
-  await page.locator('[data-test="username"]').press('Tab');
-  await page.locator('[data-test="password"]').fill('secret_sauce');
   await page.locator('[data-test="login-button"]').click();
   await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
   await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
